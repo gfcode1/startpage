46feb3ee24183cba75cd2ae6394bb065bb5bfd62
gio <gio@gfcode.it>
2026-06-11 12:34:30 +0200

    vai

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig([
-  globalIgnores(['dist', 'public/emulator']),
+  globalIgnores(['dist', 'public/emulator', 'neutralino.d.ts']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
